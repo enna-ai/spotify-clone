f30b184272051eb9dd8276eff36fac02dc2fa052
Anne <92945218+vian-ai@users.noreply.github.com>
2022-09-26 01:47:40 -0600

    Add files via upload

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,62 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
+import PublicIcon from '@mui/icons-material/Public';
+import GroupIcon from '@mui/icons-material/Group';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import { HiOutlineMenu } from 'react-icons/hi';
+import { RiCloseLine } from 'react-icons/ri';
+
+import { logo } from '../assets';
+
+const links = [
+  { name: 'Discover', to: '/', icon: HomeIcon },
+  { name: 'Around You', to: '/around-you', icon: PublicIcon },
+  { name: 'Top Artists', to: '/top-artists', icon: GroupIcon },
+  { name: 'Top Charts', to: '/top-charts', icon: BarChartIcon },
+];
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-[#7d7d7dbf] transition duration-500 hover:text-[#b3b3b3]"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
 );
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#040306]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+
+      {/* Mobile sidebar */}
+      <div className="absolute md:hidden block top-6 right-3">
+        {!mobileMenuOpen ? (
+          <HiOutlineMenu className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(true)} />
+        ) : (
+          <RiCloseLine className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(false)} />
+        )}
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#0100097f] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
+  );
+};
 
 export default Sidebar;
